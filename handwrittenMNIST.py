19ae7e1e39089d29787c3a74d83626eb6cc1c507
Susmoy Barman <susmoy.barman.bd@gmail.com>
2022-01-12 11:32:46 +0600

    Plotting added

--- handwrittenMNIST.py
+++ handwrittenMNIST.py
@@ -5,7 +5,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Activation, Input, Flatten, Dense
 from tensorflow.keras.models import Model
-
+from tensorflow.keras.callbacks import EarlyStopping, History, ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import load_model
 
 from tensorflow.keras.datasets.mnist import load_data
@@ -14,21 +15,49 @@ import matplotlib.pyplot as plt
 
 
 def main():
+	trainX, trainY, testX, testY = data_preparation()
 
-	(trainX, trainY), (testX, testY) = load_data()
+	#train_model(trainX, trainY, testX, testY)
 
-	trainX, trainY, testX, testY = data_preparation(trainX, trainY, testX, testY)
-
-	model = build_model()
-	'''
+	model = load_model("mnist_model.h5")
 	print()
 	model.summary()
-	'''
+	print()
+
+	n = 5
+	predictedY = model.predict(testX[:n])
+
+	plot_predicted_data(testX[:n], testY[:n], n, predictedY)
+
+	#print(np.argmax(predictedY))
+	#plot_single_data(testX[:1], testY[:1])
 
 	
+
+def plot_predicted_data(testX, testY, n, predictedY):
+
+	plt.figure(figsize=(20, 20))
+
+	for i in range(n):
+		print(f'\n\nPredicted Value: {np.argmax(predictedY[i])}\n')
+
+		plt.subplot(3, 3, i+1)
+		plt.imshow(testX[i], cmap = 'gray')
+		#plt.title(y[i])
+		plt.title(np.argmax(testY[i]))
+
+	plt.show()
+	plt.close()
+
+def train_model(trainX, trainY, testX, testY):
+
+	model = build_model()
 	
 	# Train model
-	model.fit(trainX, trainY, epochs = 2, validation_split = 0.2)
+	my_callbacks = [EarlyStopping(monitor='val_loss', patience=10),
+					History()]
+
+	model.fit(trainX, trainY, epochs = 100, callbacks= my_callbacks, validation_split = 0.2)
 	model.evaluate(testX, testY)
 	
 	#!mkdir -p saved_model
@@ -42,19 +71,9 @@ def main():
 	
 
 	# Calling `save('my_model.h5')` creates a h5 file `my_model.h5`.
-	#model.save("mnist_model.h5")
+	model.save("mnist_model.h5")
 
-	'''
-	model = load_model("mnist_model.h5")
-	model.summary()
-	print()
-
-	predictedY = model.predict(testX[:1])
-	print(f'\n\nPredicted Value: {np.argmax(predictedY)}\n')
-
-	#print(np.argmax(predictedY))
-	plot_single_data(testX[:1], testY[:1])
-	'''
+	
 
 
 def build_model():
@@ -68,12 +87,17 @@ def build_model():
 	output = Dense(10, activation='softmax')(fc)
 
 	model = Model(inpt, output, name='mnist_NN')
-	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics='accuracy')
+
+	opt = Adam(learning_rate=0.01)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics='accuracy')
 
 	return model
 
 
-def data_preparation(trainX, trainY, testX, testY):	
+def data_preparation():	
+
+	# Load data
+	(trainX, trainY), (testX, testY) = load_data()
 
 	# Type conversion
 	trainX = trainX.astype("float32")
@@ -99,13 +123,15 @@ def plot_single_data(x, y):
 	plt.show()
 	plt.close()
 
-def plot_data(x, y):
+
+def plot_data(x, y, n):
 	plt.figure(figsize=(20, 20))
 
-	for i in range(9):
+	for i in range(n):
 		plt.subplot(3, 3, i+1)
 		plt.imshow(x[i], cmap = 'gray')
-		plt.title(y[i])
+		#plt.title(y[i])
+		plt.title(np.argmax(y[i]))
 
 	plt.show()
 	plt.close()
@@ -113,30 +139,3 @@ def plot_data(x, y):
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-# Save the entire model as a SavedModel.
-!mkdir -p saved_model
-model.save('saved_model/my_model')
-
-
-new_model = tf.keras.models.load_model('saved_model/my_model')
-
-# Check its architecture
-new_model.summary()
-
-
-
-'''
