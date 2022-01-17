6b50ae2f6e3558e28cf20166ef104e8d91071eed
Susmoy Barman <susmoy.barman.bd@gmail.com>
2022-01-17 13:19:32 +0600

    Compilation added

--- vgg_transfer.py
+++ vgg_transfer.py
@@ -8,10 +8,68 @@ from email.mime import base
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.callbacks import EarlyStopping
 
 def main():
     model = buildModel()
+    #modelSummary(model)
+
+    imgSet = prepare_image_data()
+    labelSet = prepare_label_data()
+    trainImg, testImg, trainLabel, testLabel = split_data(imgSet, labelSet)
+
+    model.compile(
+        loss='mse',
+        optimizer='adam'
+    )
+    
+    model.fit(
+        trainImg,
+        trainLabel,
+        batch_size=8,
+        epochs=100,
+        validation_split=0.05,
+        callback = EarlyStopping(
+            monitor = 'val_loss',
+            patience=10
+        )
+    )
+
+    prediction = model.predict(testImg[:10])
+
+
+def split_data(imgSet, labelSet):
+    trainImg = imgSet[:70]
+    testImg = imgSet[70:]
+    
+    trainLabel = labelSet[:70]
+    testLabel = labelSet[70:]
+
+    return trainImg, testImg, trainLabel, testLabel
+
+
+def prepare_label_data():
+    # Prepare data for  2 classes
+    # 0: Class 1
+    # 1: Class2
+
+    # Convert labels into one-hot vectors
+    # return  one-hot vectors
+    return
+
+def prepare_image_data():
+    # Load Image
+    # resize image
+    # Put images into a 4D numpy array (n, 256, 245, 3)
+    # Preprocess the 4D numpy array according to VGG16
+    # Return the 4D numpy array dataset
+    
+    return
+
+def modelSummary(model):
+    print()
     model.summary()
+    print()
 
 def buildModel():
 
@@ -28,7 +86,7 @@ def buildModel():
     '''
     fc = Flatten()(baseModelOutput)
     fc = Dense(16, activation='relu')(fc)
-    outputs = Dense(2, activation='relu')(fc)
+    outputs = Dense(2, activation='softmax')(fc)
     
     '''
     Concatening Original model and our model
