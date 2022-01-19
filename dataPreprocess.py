183734698a3d5b7a9e497bb4db9e78aa0b7d52c1
Susmoy Barman <susmoy.barman.bd@gmail.com>
2022-01-19 12:36:57 +0600

    resized image save

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -10,36 +10,86 @@ def main():
     # Local Directory name
     car = 'car/'
     hen = 'hen/'
+    resizedCar = 'resized_car/'
+    resizedHen = 'resized_hen/'
 
-
+    # Working with Car
     print("\n----------\nPreparing Car Dataset")
     imgPrepare(car)
+    print("\n\n-----------------------\nChecking The resized Image")
+    checkResizedImage(resizedCar)
+
+    # Working with hen
     print("\n----------\nPrepare Hen Dataset")
     imgPrepare(hen)
+    print("\n\n-----------------------\nChecking The resized Image")
+    checkResizedImage(resizedHen)
 
-def imgPrepare(object):
+
+def checkResizedImage(object):
     imgDir = DIR + object
-    imgList = os.listdir(imgDir)
 
+    imgList = os.listdir(imgDir)
     n = len(imgList)
-    imgDataset = []
+
+    print(f"\nPrinting resized {object}\n")
     for i in range(n):
         imgPath = imgDir + imgList[i]
         
         if (os.path.exists(imgPath)):
             # Load Image
             img = cv2.imread(imgPath)
-            print(f"{imgPath}, img.shape: {img.shape}", end=' ')
+            print(f"{imgPath}, img.shape: {img.shape}")
+
+        else:
+            print(f"{imgPath} not exists")
+    print('\n\n')
+
+
+'''
+Data Preparation function
+'''
+def imgPrepare(object):
+    imgDir = DIR + object
+    saveDir = DIR + 'resized_' + object
+
+    # Creating new directory
+    if not os.path.isdir(saveDir):
+        print('\nThe directory is not present. Creating a new one..\n')
+        os.mkdir(saveDir)
+    else:
+        print('\nThe directory is present.\n')
+
+
+    imgList = os.listdir(imgDir)
+    n = len(imgList)
+    imgDataset = []
+    count = 1
+    for i in range(n):
+        imgPath = imgDir + imgList[i]
+        
+        if (os.path.exists(imgPath)):
+            # Load Image
+            img = cv2.imread(imgPath)
+            print(f"{imgPath}, img.shape: {img.shape}")
 
             # Resize Image
             resizedImg = cv2.resize(img, (img_size, img_size))  
-            print(f"resizedImg.shape: {resizedImg.shape}", end='\n\n')
+            #print(f"resizedImg.shape: {resizedImg.shape}", end='\n\n')
 
             # BGR to RGB
             rgbImg = cv2.cvtColor(resizedImg, cv2.COLOR_BGR2RGB)
             
             # Put the image to array
             imgDataset.append(rgbImg)
+
+            # Write Resized Image
+            imgName = object[:-1] + str(count) + '.jpg'
+            imgWritePath = saveDir + imgName
+            #print(imgWritePath, imgList[i])
+            cv2.imwrite(imgWritePath, resizedImg)
+
+            count = count + 1
 
         else:
             print(f"{imgPath} not exists")
